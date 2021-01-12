8eed04a5b89e27417606c9a16447936a6fb55c9f
reed756 <68922323+reed756@users.noreply.github.com>
2021-01-12 08:24:24 +0000

    Added mouseover listener so boxes turn black

--- script.js
+++ script.js
@@ -5,10 +5,39 @@ const container = document.querySelector('#container');
 function createGrid() {
     for (let i = 1; i <= 256; i ++) {
         let cell = document.createElement('div');
-        
+        cell.addEventListener('mouseover', (event) => {
+            cell.style.backgroundColor = "black";
+        });
         container.appendChild(cell).classList = "grid-item";
     }
 }
+
+/*
+const grid = document.querySelectorAll('span');
+grid.forEach((span) => {
+    span.addEventListener('mouseover', () => {
+        box.style.backgroundColor = "black";
+});
+*/
+
+/*
+const grid = document.querySelector('span');
+grid.addEventListener('mouseover', () => {
+    grid.style.backgroundColor = "black";
+});
+*/
+/*
+a = document.getElementsByTagName('span');
+for (i in a) {
+    a[i].onmouseover = a[i].style.backgroundColor = "black";
+}
+*/
+/*
+const box = document.getElementsByClassName('grid-item');
+box.addEventListener('mouseover', () => {
+    box.style.background = "black";
+});
+*/
 
 createGrid();
 
