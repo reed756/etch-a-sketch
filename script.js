ec55c43b6e59148a78ecce3cd7ee3b6523d3384c
reed756 <68922323+reed756@users.noreply.github.com>
2021-01-12 21:04:27 +0000

    Added a button which resets the grid

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Create 16 x 16 worth of divs. 
 
 const container = document.querySelector('#container');
+const reset = document.querySelector('button');
+const grid = document.querySelectorAll('.grid-item');
 
 function createGrid() {
     for (let i = 1; i <= 256; i ++) {
@@ -9,35 +11,10 @@ function createGrid() {
             cell.style.backgroundColor = "black";
         });
         container.appendChild(cell).classList = "grid-item";
+        reset.addEventListener('click', (event) => {
+            cell.style.backgroundColor = "white";
+        })
     }
 }
 
-/*
-const grid = document.querySelectorAll('span');
-grid.forEach((span) => {
-    span.addEventListener('mouseover', () => {
-        box.style.backgroundColor = "black";
-});
-*/
-
-/*
-const grid = document.querySelector('span');
-grid.addEventListener('mouseover', () => {
-    grid.style.backgroundColor = "black";
-});
-*/
-/*
-a = document.getElementsByTagName('span');
-for (i in a) {
-    a[i].onmouseover = a[i].style.backgroundColor = "black";
-}
-*/
-/*
-const box = document.getElementsByClassName('grid-item');
-box.addEventListener('mouseover', () => {
-    box.style.background = "black";
-});
-*/
-
 createGrid();
-
