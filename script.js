41e446ee8be71f2856577c2888f25376dbdf6d31
reed756 <68922323+reed756@users.noreply.github.com>
2021-01-16 11:46:32 +0000

    added functionality that clears grid

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 const container = document.querySelector('#container');
 const reset = document.querySelector('button');
 const grid = document.querySelectorAll('.grid-item');
+let cell = document.createElement('div');
 
 function createGrid() {
-    for (let i = 1; i <= 256; i ++) {
+    for (let i = 1; i <= 256; i++) {
         let cell = document.createElement('div');
         cell.addEventListener('mouseover', (event) => {
             cell.style.backgroundColor = "black";
@@ -16,5 +17,24 @@ function createGrid() {
         })
     }
 }
+/*
+reset.addEventListener('click', (event) => {
+    reset.addEventListener('click', (event) => {
+        container.removeChild(cell);
+    })
+    newGrid();
+})
+*/
+function newGrid() {
+    for (let i = 1; i <= 256; i++) {
+        let newCell = document.createElement('div');
+        container.appendChild(newCell).classList = "new-grid";
+    }
+}
 
 createGrid();
+/*
+reset.addEventListener('click', (event) => {
+    cell.style.backgroundColor = "white";
+})
+*/
