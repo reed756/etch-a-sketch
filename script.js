58b4b633086115d2c3c2182481c4136225fa264e
reed756 <68922323+reed756@users.noreply.github.com>
2021-01-17 00:02:28 +0000

    added function to reset button that requests number and creates grid of that size

--- script.js
+++ script.js
@@ -2,21 +2,37 @@
 
 const container = document.querySelector('#container');
 const reset = document.querySelector('button');
-const grid = document.querySelectorAll('.grid-item');
-let cell = document.createElement('div');
 
 function createGrid() {
     for (let i = 1; i <= 256; i++) {
+        document.documentElement.style.setProperty("--columns-row", 16);
         let cell = document.createElement('div');
         cell.addEventListener('mouseover', (event) => {
             cell.style.backgroundColor = "black";
         });
         container.appendChild(cell).classList = "grid-item";
-        reset.addEventListener('click', (event) => {
-            cell.style.backgroundColor = "white";
-        })
     }
 }
+
+function resetGrid() {
+    reset.addEventListener('click', (event) => {
+    container.innerHTML = '';
+    let answer = prompt('type a number between 1 and 100', '');
+    document.documentElement.style.setProperty("--columns-row", answer);
+    for (let i = 1; i <= answer * answer; i++) {
+        let newCell = document.createElement('div');
+        newCell.addEventListener('mouseover', (event) => {
+            newCell.style.backgroundColor = "black";
+        });
+        container.appendChild(newCell).classList = "new-grid";
+    }
+    });
+}
+
+createGrid();
+
+reset.addEventListener('click', resetGrid());
+
 /*
 reset.addEventListener('click', (event) => {
     reset.addEventListener('click', (event) => {
@@ -25,16 +41,11 @@ reset.addEventListener('click', (event) => {
     newGrid();
 })
 */
+/*
 function newGrid() {
     for (let i = 1; i <= 256; i++) {
         let newCell = document.createElement('div');
         container.appendChild(newCell).classList = "new-grid";
     }
 }
-
-createGrid();
-/*
-reset.addEventListener('click', (event) => {
-    cell.style.backgroundColor = "white";
-})
 */
